e96292931cb94b9555fcd72c135115e9fbae7166
zviad <zviad.nemsadze@magticom.ge>
2022-05-16 21:32:24 +0400

    initial commit

---
+++ src/main/java/ge/magticom/appdynamics/SimpleHello.java
@@ -0,0 +1,15 @@
+package ge.magticom.appdynamics;
+
+
+import org.apache.log4j.Logger;
+
+
+
+public class SimpleHello {
+
+    public static  Logger logger=Logger.getLogger(SimpleHello.class);
+
+    public static void main(String[] argv){
+        logger.info("test12222");
+    }
+}
