f0fead81df392d67f32c4b0a6cea6025714bfd4b
zviad <zviad.nemsadze@magticom.ge>
2022-05-17 15:04:19 +0400

    CCare EntryPoint,Mymagti exitpoint

--- src/main/java/ge/magticom/appdynamics/SimpleHello.java
+++ src/main/java/ge/magticom/appdynamics/SimpleHello.java
@@ -1,15 +1,9 @@
 package ge.magticom.appdynamics;
 
-
 import org.apache.log4j.Logger;
-
-
-
 public class SimpleHello {
-
     public static  Logger logger=Logger.getLogger(SimpleHello.class);
-
     public static void main(String[] argv){
-        logger.info("test12222");
+        logger.info("test1222112");
     }
 }
