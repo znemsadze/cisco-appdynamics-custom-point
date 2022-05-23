1e0b53d2c5f4a30cddadcbfc344375979e33fcdc
zviad <zviad.nemsadze@magticom.ge>
2022-05-23 15:22:27 +0400

    CCare EntryPoint,Mymagti exitpoint

--- src/main/java/ge/magticom/appdynamics/MymagtiInterceptor.java
+++ src/main/java/ge/magticom/appdynamics/MymagtiInterceptor.java
@@ -21,7 +21,6 @@ public class MymagtiInterceptor extends AGenericInterceptor {
     @Override
     public List<Rule> initializeRules() {
         System.out.println("MymagtiInterceptor initializeRules");
-        System.out.println("Pittaloooooooo=========================================MymagtiInterceptor initializeRules");
         Rule.Builder bldr = new Rule.Builder(CLASS_TO_INSTRUMENT);
         bldr = bldr.classMatchType(SDKClassMatchType.MATCHES_CLASS).classStringMatchType(SDKStringMatchType.EQUALS);
         bldr = bldr.methodMatchString(METHOD_TO_INSTRUMENT).methodStringMatchType(SDKStringMatchType.EQUALS);
